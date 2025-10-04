85a7ef96dccde2d01d32a4397b279268536b4bea
Raghuvansh <raghuvanshmani876@gmail.com>
2025-10-04 18:00:12 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,9 +1,5 @@
 import { FirebaseClientProvider } from '@/firebase';
 import { Toaster } from '@/components/ui/toaster';
-import { cn } from '@/lib/utils';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 export default function LoginLayout({
   children,
@@ -11,18 +7,9 @@ export default function LoginLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="dark" suppressHydrationWarning>
-       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
-      </head>
-      <body className={cn('min-h-screen bg-background font-body antialiased', inter.variable)}>
-        <FirebaseClientProvider>
-          {children}
-          <Toaster />
-        </FirebaseClientProvider>
-      </body>
-    </html>
+    <FirebaseClientProvider>
+      {children}
+      <Toaster />
+    </FirebaseClientProvider>
   );
 }
