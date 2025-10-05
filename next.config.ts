126b49447ce55f4d2bc23899b285fdce37ab991a
Raghuvansh <raghuvanshmani876@gmail.com>
2025-10-05 10:43:44 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,10 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "storage.googleapis.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       }
     ],
   },
